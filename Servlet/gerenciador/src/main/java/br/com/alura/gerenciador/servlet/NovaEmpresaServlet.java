fbeb8ea3988731632ed63ea60daf39bcb30dd202
Lediel da Rocha Delfino <lediel.delfino@betha.com.br>
2023-09-23 12:03:14 -0300

    Redirecionamento pelo navegador

--- Servlet/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ Servlet/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -5,7 +5,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -42,9 +41,12 @@ public class NovaEmpresaServlet extends HttpServlet {
 		
 		banco.adiciona(empresa);
 		
-		RequestDispatcher rd = req.getRequestDispatcher("/listaEmpresas");
 		req.setAttribute("empresa", empresa.getNome());
-		rd.forward(req, res);
+		res.sendRedirect("listaEmpresas");
+		
+		/*RequestDispatcher rd = req.getRequestDispatcher("/listaEmpresas");
+		req.setAttribute("empresa", empresa.getNome());
+		rd.forward(req, res);*/
 	}
 
 }
