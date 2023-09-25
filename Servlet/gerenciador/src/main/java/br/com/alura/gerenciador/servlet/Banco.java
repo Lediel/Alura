7bbe71fa6bf3d06bec85135e5225d37c18c38b60
Lediel da Rocha Delfino <lediel.delfino@betha.com.br>
2023-09-25 07:43:54 -0300

    Removendo empresa

--- Servlet/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ Servlet/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -6,11 +6,14 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>(); 
+	private static Integer chaveSequencial = 1;
 
 	static {
 		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
 		empresa.setNome("Alura");
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Caelum");
 		
 		lista.add(empresa);
@@ -18,11 +21,27 @@ public class Banco {
 	}
 	
 	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
 		Banco.lista.add(empresa);
 	}
 	
 	public List<Empresa> getEmpresas() {
 		return Banco.lista;
+	}
+
+	public void removeEmpresa(Integer id) {
+		
+		lista.removeIf(emp -> emp.getId().equals(id));
+		
+		/*Iterator<Empresa> it = lista.iterator();
+		
+		while (it.hasNext()) {
+			Empresa emp = it.next();
+			if (emp.getId() == id) {
+				it.remove();
+			}
+		}*/
+		
 	}
 
 }
--- Servlet/gerenciador/src/main/webapp/listaEmpresas.jsp
+++ Servlet/gerenciador/src/main/webapp/listaEmpresas.jsp
@@ -15,11 +15,16 @@
 		<c:if test="${not empty empresa}">
 			Empresa ${empresa} cadastrada com sucesso!
 		</c:if>
+		
+		Lista de empresas: <br/>
 	
 		<ul>
 			<c:forEach items="${empresas}" var="empresa">
 			  	 
-				<li>${empresa.nome} - <fmt:formatDate value="${empresa.dataAbertura}" pattern="dd/MM/yyyy"/></li>
+				<li>
+					${empresa.nome} - <fmt:formatDate value="${empresa.dataAbertura}" pattern="dd/MM/yyyy"/>
+					<a href="/gerenciador/removeEmpresa?id=${empresa.id}">Remover</a>
+				</li>
 			</c:forEach>
 		</ul>
 	</body>
