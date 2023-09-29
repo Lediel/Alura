62b1a44cff62cfaeaa60b5086bed973b625943a1
Lediel <ledielrd@gmail.com>
2023-09-29 08:03:20 -0300

    Escalabilidade de conexões

--- JDBC/byte-bank-alura/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ JDBC/byte-bank-alura/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -1,5 +1,8 @@
 package br.com.alura.bytebank;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,9 +12,18 @@ public class ConnectionFactory {
     public Connection recuperarConexao() {
 
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=");
+            return createDataSource().getConnection();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+    private HikariDataSource createDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank");
+        config.setUsername("root");
+        config.setPassword("");
+        config.setMaximumPoolSize(10);
+
+        return new HikariDataSource(config);
     }
 }
