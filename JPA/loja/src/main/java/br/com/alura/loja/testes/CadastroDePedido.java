e86af98931e95422ce5ad402edacf1b814d44f52
Lediel <ledielrd@gmail.com>
2023-10-09 07:46:19 -0300

    Consultas para relatórios

--- JPA/loja/src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
+++ JPA/loja/src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
@@ -9,6 +9,7 @@ import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class CadastroDePedido {
 
@@ -37,6 +38,13 @@ public class CadastroDePedido {
 
         BigDecimal totalVendido = pedidoDao.valorTotalVendido();
         System.out.println("Valor Total: " + totalVendido);
+
+        List<Object[]> relatorio = pedidoDao.relatorioDeVendas();
+        for (Object[] obj : relatorio) {
+            System.out.println(obj[0]);
+            System.out.println(obj[1]);
+            System.out.println(obj[2]);
+        }
     }
 
     private static void popularBancoDeDados() {
