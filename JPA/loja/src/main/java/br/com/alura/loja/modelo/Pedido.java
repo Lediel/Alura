8c2a7953e8bb07428d044841e52082bce0a3a903
Lediel <ledielrd@gmail.com>
2023-10-10 08:05:08 -0300

    Entendendo Lazy, Eager e Join Fetch

--- JPA/loja/src/main/java/br/com/alura/loja/modelo/Pedido.java
+++ JPA/loja/src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -16,7 +16,7 @@ public class Pedido {
     @Column(name = "valor_total")
     private BigDecimal valorTotal = BigDecimal.ZERO;
     private LocalDate data = LocalDate.now();
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Cliente cliente;
 
     @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
@@ -56,5 +56,9 @@ public class Pedido {
 
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
     }
 }
