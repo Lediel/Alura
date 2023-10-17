75f24b3657f536da4630c853e3600c9c68c6fa10
Lediel <ledielrd@gmail.com>
2023-10-17 07:59:40 -0300

    Salvando o registro

--- spring-data/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ spring-data/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -2,17 +2,22 @@ package br.com.alura.spring.data;
 
 import br.com.alura.spring.data.orm.Cargo;
 import br.com.alura.spring.data.repository.CargoRepository;
+import br.com.alura.spring.data.service.CrudCargoService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
 
-	private final CargoRepository repository;
+	private final CrudCargoService cargoService;
 
-	public SpringDataApplication(CargoRepository repository) {
-		this.repository = repository;
+	private Boolean system = true;
+
+	public SpringDataApplication(CrudCargoService cargoService) {
+		this.cargoService = cargoService;
 	}
 
 	public static void main(String[] args) {
@@ -21,9 +26,23 @@ public class SpringDataApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Cargo cargo = new Cargo();
-		cargo.setDescricao("DESENVOLVEDOR DE SOFWARE");
 
-		repository.save(cargo);
+		Scanner scanner = new Scanner(System.in);
+
+		while (system) {
+			System.out.println("Qual ação você quer executar");
+
+			System.out.println("0 - Sair");
+			System.out.println("1 - Cargo");
+
+			int action = scanner.nextInt();
+
+			if (action == 1) {
+				cargoService.inicial(scanner);
+			} else {
+				system = false;
+				System.out.println("Finalizando!");
+			}
+		}
 	}
 }
