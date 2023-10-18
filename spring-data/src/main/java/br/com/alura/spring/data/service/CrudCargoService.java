cb74211a9fec038636f5ef73cf63ca03dd122ff1
Lediel <ledielrd@gmail.com>
2023-10-18 08:02:25 -0300

    Vizualizar e deletar

--- spring-data/src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
+++ spring-data/src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
@@ -22,6 +22,8 @@ public class CrudCargoService {
             System.out.println("0 - Sair");
             System.out.println("1 - Salvar");
             System.out.println("2 - Atualizar");
+            System.out.println("3 - Vizualizar");
+            System.out.println("4 - Deletar");
 
             int action = scanner.nextInt();
 
@@ -32,12 +34,20 @@ public class CrudCargoService {
                 case 2:
                     atualizar(scanner);
                     break;
+                case 3:
+                    visualizar();
+                    break;
+                case 4:
+                    deletar(scanner);
+                    break;
                 default:
                     system = false;
                     break;
             }
         }
     }
+
+
 
     private void salvar(Scanner scanner) {
         System.out.println("Descrição do cargo");
@@ -60,6 +70,17 @@ public class CrudCargoService {
 
         cargoRepository.save(cargo);
         System.out.println("Salvo!");
+    }
 
+    private void visualizar() {
+        Iterable<Cargo> cargos = cargoRepository.findAll();
+        cargos.forEach(cargo -> System.out.println(cargo));
+    }
+
+    private void deletar(Scanner scanner) {
+        System.out.println("Id");
+        int id = scanner.nextInt();
+        cargoRepository.deleteById(id);
+        System.out.println("Deletado!");
     }
 }
