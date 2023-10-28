4ed700e57ca9f5fb2b6dd3a4429e4cbae12d362e
Lediel <ledielrd@gmail.com>
2023-10-28 12:07:16 -0300

    Nova Migration

--- spring-boot/med-voll-api/src/main/java/med/voll/api/medico/Medico.java
+++ spring-boot/med-voll-api/src/main/java/med/voll/api/medico/Medico.java
@@ -15,10 +15,12 @@ import med.voll.api.endereco.Endereco;
 @EqualsAndHashCode(of = "id")
 public class Medico {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
+    private String telefone;
     private String crm;
 
     @Enumerated(EnumType.STRING)
@@ -30,6 +32,7 @@ public class Medico {
     public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
         this.email = dados.email();
+        this.telefone = dados.telefone();
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
