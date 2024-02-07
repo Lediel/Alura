00fa5e879639a2492f9d07e21d05f4d399b0d4d2
Lediel <ledielrd@gmail.com>
2024-02-07 16:39:27 -0300

    Extraindo validações

--- SOLID/rh/src/br/com/alura/rh/service/ValidacaoPeridiocidadeEntreReajuste.java
+++ SOLID/rh/src/br/com/alura/rh/service/ValidacaoPeridiocidadeEntreReajuste.java
@@ -8,28 +8,15 @@ import java.time.temporal.ChronoUnit;
 import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
 
-public class ReajusteService {
+public class ValidacaoPeridiocidadeEntreReajuste implements ValidacaoReajuste {
 	
-	public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
-		
-		BigDecimal salarioAtual = funcionario.getSalario();
-		BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
-		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-		}
-		
+	public void validar(Funcionario funcionario, BigDecimal aumento) {
 		LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
 		LocalDate dataAtual = LocalDate.now();
 		long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
 		if (mesesDesdeUltimoReajuste < 6) {
 			throw new ValidacaoException("Intervalo entre reajustes deve ser de no minimo 6 meses");
 		}
-		
-		BigDecimal salarioReajustado = salarioAtual.add(aumento);
-		funcionario.atualizarSalario(salarioReajustado);
-		
-		
 	}
-	
 	
 }
