44c4fa4648386b63ee11ea3896db0c787faac769
Lediel <ledielrd@gmail.com>
2024-02-09 07:10:11 -0300

    Finalizando

--- SOLID/rh/src/br/com/alura/rh/model/Terceirizado.java
+++ SOLID/rh/src/br/com/alura/rh/model/Terceirizado.java
@@ -1,35 +1,24 @@
 package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
-public class Funcionario {
+public class Terceirizado {
 
-	
-	private LocalDate dataUltimoReajuste;
+	private String empresa;
 	private DadosPessoais dadosPessoais;
-
-	public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario) {
+	
+	public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario) {
 		this.dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
 	}
-
-	public void atualizarSalario(BigDecimal novoSalario) {
-		this.dadosPessoais.setSalario(novoSalario);
-		this.dataUltimoReajuste = LocalDate.now();
+	
+	public String getEmpresa() {
+		return empresa;
 	}
 
-	public void promover(Cargo novoCargo) {
-		this.dadosPessoais.setCargo(novoCargo);
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
 	}
 	
-	public LocalDate getDataUltimoReajuste() {
-		return dataUltimoReajuste;
-	}
-
-	public void setDataUltimoReajuste(LocalDate dataUltimoReajuste) {
-		this.dataUltimoReajuste = dataUltimoReajuste;
-	}
-
 	public BigDecimal getSalario() {
 		return dadosPessoais.getSalario();
 	}
@@ -57,6 +46,5 @@ public class Funcionario {
 	public void setCargo(Cargo cargo) {
 		this.dadosPessoais.setCargo(cargo);
 	}
-	
 
 }
