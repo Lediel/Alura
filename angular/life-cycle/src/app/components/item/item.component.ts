87ba082f13c579dad0b6dc7777667ee5cafa96e9
Lediel <ledielrd@gmail.com>
2024-02-23 15:29:56 -0300

    @Output(): comunicação de filho para pai

--- angular/life-cycle/src/app/components/item/item.component.ts
+++ angular/life-cycle/src/app/components/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Item } from 'src/app/interfaces/iItem';
 
@@ -10,6 +10,7 @@ import { Item } from 'src/app/interfaces/iItem';
 export class ItemComponent implements OnInit, OnChanges {
 
   @Input() item!: Item;
+  @Output() emitindoItemParaEditar = new EventEmitter();
 
   faPen = faPen;
   faTrash = faTrash
@@ -17,11 +18,13 @@ export class ItemComponent implements OnInit, OnChanges {
   constructor() { }
 
   ngOnChanges(): void {
-    console.log("onChanges")
   }
 
   ngOnInit(): void {
-    console.log("onInit")
+  }
+
+  editarItem() {
+    this. emitindoItemParaEditar.emit(this.item);
   }
 
 }
