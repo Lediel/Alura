58d29ec7ed573f39eda98d53a5de7db3f57cf4a9
Lediel <ledielrd@gmail.com>
2024-03-06 07:32:56 -0300

    Next, error e complete

--- angular/rxjs/src/app/views/lista-livros/lista-livros.component.ts
+++ angular/rxjs/src/app/views/lista-livros/lista-livros.component.ts
@@ -16,12 +16,14 @@ export class ListaLivrosComponent {
   ) { }
 
   buscarLivros() {
-    this.service.buscar(this.campoBusca).subscribe(
-      (retornoAPI) => console.log(retornoAPI)
-    )
+    this.service.buscar(this.campoBusca).subscribe({
+      next: retornoAPI => console.log(retornoAPI),
+      error: erro => console.error(erro),
+      complete: () => console.log("Observable completado.")
+    })
+
   }
 
+
+
 }
-
-
-
