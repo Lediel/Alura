fce0da9a48fdaabb41f429a9cb9d0eabcde1d2a8
Lediel <ledielrd@gmail.com>
2024-03-08 07:41:38 -0300

    Pipe async

--- angular/rxjs/src/app/views/lista-livros/lista-livros.component.ts
+++ angular/rxjs/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Subscription, map, switchMap } from 'rxjs';
-import { Item, Livro } from 'src/app/models/interfaces';
+import { map, switchMap, tap } from 'rxjs';
+import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -10,43 +10,25 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy {
+export class ListaLivrosComponent {
 
-  listaLivros: Livro[];
   campoBusca = new FormControl()
-  subscription: Subscription
-  livro: Livro
 
   constructor(
     private service: LivroService
   ) { }
 
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    tap(() => console.log("Fluxo inicial")),
     switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
-    map((items) => this.listaLivros = this.livrosReultadoParaLivros(items))
+    tap(() => console.log("Requisição ao servidor")),
+    map((items) => this.livrosReultadoParaLivros(items))
   )
-
-  // buscarLivros() {
-  //   this.subscription = this.service.buscar(this.campoBusca).subscribe({
-  //     next: (items) => {
-  //       this.listaLivros = this.livrosReultadoParaLivros(items)
-  //     },
-  //     error: erro => console.error(erro),
-  //   })
-
-  // }
 
   livrosReultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
     return items.map(item => {
       return new LivroVolumeInfo(item)
     })
-  }
-
-
-
-  ngOnDestroy(): void {
-    console.log("ENTROU DESTROY")
-    this.subscription.unsubscribe()
   }
 
 }
